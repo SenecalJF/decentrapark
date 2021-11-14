894fa28a9e6fb9677a42a759b26bbd5e6a900a8c
hainsdominic <dominic@hainstech.com>
2021-11-13 22:32:07 -0500

    chore(config files): setup

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -46,50 +46,6 @@ export default ({ drizzle, drizzleState }) => {
       </div>
 
       <div className="section">
-        <h2>TutorialToken</h2>
-        <p>
-          Here we have a form with custom, friendly labels. Also note the token
-          symbol will not display a loading indicator. We've suppressed it with
-          the <code>hideIndicator</code> prop because we know this variable is
-          constant.
-        </p>
-        <p>
-          <strong>Total Supply: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="totalSupply"
-            methodArgs={[{ from: drizzleState.accounts[0] }]}
-          />{" "}
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="symbol"
-            hideIndicator
-          />
-        </p>
-        <p>
-          <strong>My Balance: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="TutorialToken"
-            method="balanceOf"
-            methodArgs={[drizzleState.accounts[0]]}
-          />
-        </p>
-        <h3>Send Tokens</h3>
-        <ContractForm
-          drizzle={drizzle}
-          contract="TutorialToken"
-          method="transfer"
-          labels={["To Address", "Amount to Send"]}
-        />
-      </div>
-
-      <div className="section">
         <h2>ComplexStorage</h2>
         <p>
           Finally this contract shows data types with additional considerations.
