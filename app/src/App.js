f8683edc5989d078a92c31264291f783ff2c6f81
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-11-17 08:23:38 -0500

    ci(APP): deleted files

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { DrizzleContext } from "@drizzle/react-plugin";
-import { Drizzle } from "@drizzle/store";
-import drizzleOptions from "./drizzleOptions";
-import MyComponent from "./MyComponent";
-import "./App.css";
+import React from 'react';
+import { DrizzleContext } from '@drizzle/react-plugin';
+import { Drizzle } from '@drizzle/store';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import MyComponent from './MyComponent';
+import './App.css';
 
 const drizzle = new Drizzle(drizzleOptions);
 
@@ -11,20 +12,22 @@ const App = () => {
   return (
     <DrizzleContext.Provider drizzle={drizzle}>
       <DrizzleContext.Consumer>
-        {drizzleContext => {
+        {(drizzleContext) => {
           const { drizzle, drizzleState, initialized } = drizzleContext;
 
           if (!initialized) {
-            return "Loading..."
+            return 'Loading...';
           }
 
           return (
-            <MyComponent drizzle={drizzle} drizzleState={drizzleState} />
-          )
+            <Router>
+              <Routes></Routes>
+            </Router>
+          );
         }}
       </DrizzleContext.Consumer>
     </DrizzleContext.Provider>
   );
-}
+};
 
 export default App;
