1af557fa589a61a8b8d2d56bcfc731ea3ba91e8c
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-11-21 22:49:01 -0500

    feat(app): parking modal

--- app/src/components/Parkings.js
+++ app/src/components/Parkings.js
@@ -3,7 +3,20 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Modal from '@material-ui/core/Modal';
 import { CssBaseline, Container, Typography } from '@material-ui/core';
+
+const getModalStyle = {
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  // width: 400,
+  // bgcolor: 'background.paper',
+  // border: '2px solid #000',
+  // boxShadow: 24,
+  // p: 4,
+};
 
 const styles = makeStyles((theme) => ({
   root: {
@@ -21,26 +34,70 @@ const styles = makeStyles((theme) => ({
     width: '15%',
     height: '5%',
     margin: theme.spacing(1),
+    // background: theme.palette.text.secondary,
+  },
+  paper2: {
+    position: 'absolute',
+    height: 600,
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 }));
 
 const Parkings = ({ drizzle, drizzleState }) => {
   const classes = styles();
-  const [parkingsList, setParkinksList] = useState([]);
+  const [parkingsList, setParkingsList] = useState([]);
+  const [modalStyle] = useState(getModalStyle);
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => {
+    setOpenModal(true);
+  };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
 
+  let park = [];
   useEffect(() => {
     (async () => {
-      setParkinksList(await drizzle.contracts.Decentrapark.methods.getParkings().call());
+      setParkingsList(await drizzle.contracts.Decentrapark.methods.getParkings().call());
     })();
+
+    // setParkingsList(
+    //   parkingsList.map((parking, index) => {
+    //     return { ...parking, color: showColor(index) };
+    //   })
+    // );
   }, []);
 
   const showColor = async (_index) => {
     let not_occupied = await drizzle.contracts.Decentrapark.methods.getAvailability(_index).call();
     if (not_occupied) {
-      return '#00FF00';
+      return 'primary';
     } else {
-      return '#FF0000';
+      return 'secondary';
     }
+  };
+
+  const openParking = (_index) => {
+    return (
+      <div>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={openModal}
+          onClose={handleClose}
+        >
+          <div style={modalStyle} className={classes.paper2}>
+            <Typography>Hello</Typography>
+          </div>
+        </Modal>
+      </div>
+    );
   };
 
   return (
@@ -55,22 +112,27 @@ const Parkings = ({ drizzle, drizzleState }) => {
             </Typography>
             <Grid container item xs={12}>
               {parkingsList.map((parkingStruct, index) => (
-                <Paper
-                  // color={showColor(index)}
-                  className={classes.paper}
-                  onClick={() => {
-                    alert(parkingStruct.owner);
-                  }}
-                >
-                  <Grid className={classes.item} item xs={12} align="center">
-                    {/* <Typography> {parkingStruct.owner} </Typography> */}
-                    {/* <Typography> {parkingStruct.rentPrice} </Typography> */}
-                    {/* <Typography> {parkingStruct.rentDuration / 86400} </Typography> */}
-                    {/* <Typography> {parkingStruct.parkingPrice} </Typography> */}
+                <div>
+                  <Paper
+                    // classes={{ background: showColor(index) }}
+                    variant="outlined"
+                    className={classes.paper}
+                    onClick={() => {
+                      handleOpen();
+                    }}
+                  >
+                    {/* {showColor(index)} */}
+                    <Grid className={classes.item} item xs={12} align="center">
+                      {/* <Typography> {parkingStruct.owner} </Typography> */}
+                      {/* <Typography> {parkingStruct.rentPrice} </Typography> */}
+                      {/* <Typography> {parkingStruct.rentDuration / 86400} </Typography> */}
+                      {/* <Typography> {parkingStruct.color} </Typography> */}
 
-                    <Typography> {index} </Typography>
-                  </Grid>
-                </Paper>
+                      <Typography> {index} </Typography>
+                    </Grid>
+                  </Paper>
+                  {openParking(index)}
+                </div>
               ))}
             </Grid>
           </Grid>
